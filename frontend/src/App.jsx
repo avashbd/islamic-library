0fa6cc2d2c34719f51f79c1706cf54eb0bbd46b2
avashbd <avash.bd@gmail.com>
2026-07-25 20:18:48 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import PasswordGate from "./components/PasswordGate";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import AddBook from "./pages/AddBook";
+import BulkAdd from "./pages/BulkAdd";
 import BookDetail from "./pages/BookDetail";
+import BrowseFilter from "./pages/BrowseFilter";
 import Stats from "./pages/Stats";
 import Settings from "./pages/Settings";
 
@@ -17,7 +19,9 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/add" element={<AddBook />} />
+            <Route path="/bulk-add" element={<BulkAdd />} />
             <Route path="/book/:id" element={<BookDetail />} />
+            <Route path="/browse/:type/:value" element={<BrowseFilter />} />
             <Route path="/stats" element={<Stats />} />
             <Route path="/settings" element={<Settings />} />
           </Routes>
